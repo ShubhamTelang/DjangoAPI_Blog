155594ac71f0dbd1fd31ddb0f8be91a78c4da6b0
Shubham Telang <shubhamtelang010@gmail.com>
2022-05-03 18:38:29 +0530

    Update serializers.py

--- Blogapp/api/serializers.py
+++ Blogapp/api/serializers.py
@@ -6,22 +6,3 @@ class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = "__all__"
-
-# class BlogPostSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     author_name = serializers.CharField(read_only=True)
-#     title = serializers.CharField()
-#     body = serializers.CharField()
-#     created_at = serializers.DateTimeField()
-#     updated_at = serializers.DateTimeField()
-#
-#     def create(self,validated_data):
-#         return BlogPost.objects.create(**validated_data)
-#
-#     def update(self,instance,validated_data):
-#         instance.title = validated_data.get('title',instance.title)
-#         instance.body = validated_data.get('body',instance.body)
-#         instance.created_at = validated_data.get('created_at',instance.created_at)
-#         instance.updated_at = validated_data.get('updated_at',instance.updated_at)
-#         instance.save()
-#         return instance
